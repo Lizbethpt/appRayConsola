d0608448c5018c6996580115894aa8fcc8229361
Lizbeth Peña Torres <89703185+Lizbethpt@users.noreply.github.com>
2021-10-19 13:45:16 -0500

    Add files via upload

---
+++ appRayConsola/appRayConsola/Papeleria.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appRayConsola
+{
+    class Papeleria
+    {
+        private string colordeLustrina;
+        private string pluma;
+        private string cuaderno;
+
+        public string Cuaderno
+        {
+            get { return cuaderno; }
+            set { cuaderno = value; }
+        }
+
+        public string Pluma
+        {
+            get { return pluma; }
+            set { pluma = value; }
+        }
+
+        public string ColordeLustrina
+        {
+            get { return colordeLustrina; }
+            set { colordeLustrina = value; }
+        }
+
+
+        public Papeleria()
+        {
+            colordeLustrina = "";
+            pluma = "";
+            cuaderno = "";
+        }
+        public Papeleria(string colordeLustrina, string pluma, string cuaderno)
+        {
+            this.colordeLustrina = colordeLustrina;
+            this.pluma = pluma;
+            this.cuaderno = cuaderno;
+        }
+        public override string ToString()
+        {
+            return colordeLustrina + " " + pluma + " " + cuaderno;
+        }
+
+    }
+}
---
+++ appRayConsola/appRayConsola/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appRayConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Papeleria a = new Papeleria();
+            Papeleria[]arreglopape = new Papeleria[5];
+            for (int i = 0; i < arreglopape.Length; i++)
+            {
+                arreglopape[i] = new Papeleria();
+                Console.WriteLine("Ingresa el color del lustrina");
+                arreglopape[i].ColordeLustrina = " ";
+                a.ColordeLustrina = Console.ReadLine();
+                Console.WriteLine("Ingresa el tipo y color de la pluma");
+                arreglopape[i].Pluma = " ";
+                a.Pluma = Console.ReadLine();
+                Console.WriteLine("Ingresa tipo de cuaderno");
+                arreglopape[i].Cuaderno = " ";
+                a.Cuaderno = Console.ReadLine();
+                
+            }
+            for (int i = 0; i < arreglopape.Length; i++)
+            {
+                Console.WriteLine(arreglopape[i].ToString());
+            }
+           
+            Console.ReadKey();
+
+        }
+			
+
+        
+    }     
+} 
+
